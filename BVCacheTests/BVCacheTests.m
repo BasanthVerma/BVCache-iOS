bae531fcf9e22efe406e0b1679140f84c43987b4
Basanth Verma <basanth.verma@razorthink.net>
2017-01-30 18:48:20 +0530

    Initial Commit

---
+++ BVCacheTests/BVCacheTests.m
@@ -0,0 +1,39 @@
+//
+//  BVCacheTests.m
+//  BVCacheTests
+//
+//  Created by Basanth Verma on 30/01/17.
+//  Copyright © 2017 Basanth. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BVCacheTests : XCTestCase
+
+@end
+
+@implementation BVCacheTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
